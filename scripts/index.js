45410677f8d8650de4f8b8c4e4288264c1948780
Nikolay Strelnikov <nikolaystrelnikov11@gmail.com>
2024-07-11 23:38:44 +0300

    v0.1 Beta

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,28 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placeList = document.querySelector('.places__list');
+const template = document.querySelector('#card-template').content;
+
+function createCard(item, deleteCard) {
+  const card = template.cloneNode(true);
+  const deleteButton = card.querySelector('.card__delete-button');
+
+  card.querySelector('.card__title').textContent = item.name;
+  card.querySelector('.card__image').src = item.link;
+
+  deleteButton.addEventListener('click', deleteCard);
+
+  return card;
+};
+
+function deleteCard(event) {
+  event.target.closest('.card').remove();
+};
+
+initialCards.forEach((item) => {
+  const newCard = createCard(item, deleteCard);
+  placeList.append(newCard);
+
+});
